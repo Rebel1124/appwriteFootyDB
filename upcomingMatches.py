85a1eeeb038e64bd85f4c70616399cb9b68f9b25
Desigan Reddy <78895361+Rebel1124@users.noreply.github.com>
2025-02-18 17:01:47 +0200

    Update upcomingMatches.py

--- upcomingMatches.py
+++ upcomingMatches.py
@@ -196,35 +196,6 @@ def main(context):
         
         return column_groups
     
-    # def get_all_document_ids(db_id, db_collection):
-    #     document_ids = []
-    #     offset = 0
-    #     limit = 100
-        
-    #     try:
-    #         while True:
-    #             response = databases.list_documents(
-    #                 database_id=db_id,
-    #                 collection_id=db_collection,
-    #                 queries=[
-    #                     Query.limit(limit),
-    #                     Query.offset(offset)
-    #                 ]
-    #             )
-                
-    #             if not response['documents']:
-    #                 break
-                
-    #             # document_ids.extend([doc['$id'] for doc in response['documents']])
-    #             document_ids.extend([doc['id'] for doc in response['documents']])
-    #             offset += limit
-                
-    #         return document_ids
-            
-    #     except Exception as e:
-    #         context.log(f"An error occurred: {str(e)}")
-    #         return None
-
     def get_all_document_ids(db_id, db_collection):
         document_ids = []
         last_id = None
