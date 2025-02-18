437ec68b500ad6ea491f81f91329e09708884d9d
Desigan Reddy <78895361+Rebel1124@users.noreply.github.com>
2025-02-18 15:14:41 +0200

    Update upcomingMatches.py

--- upcomingMatches.py
+++ upcomingMatches.py
@@ -15,10 +15,10 @@ def main(context):
 
     client = Client()
 
-    project_id = os.environ('APPWRITE_PROJECT_ID')
-    api_key = os.environ('APPWRITE_API_KEY')
-    database_id = os.environ('APPWRITE_DB_ID')
-    upcomingMatches_collection_id = os.environ('UPCOMING_MATCHES_COLLECTION_ID')
+    project_id = os.environ['APPWRITE_PROJECT_ID']
+    api_key = os.environ['APPWRITE_API_KEY']
+    database_id = os.environ['APPWRITE_DB_ID']
+    upcomingMatches_collection_id = os.environ['UPCOMING_MATCHES_COLLECTION_ID']
     
     client = (client
         .set_endpoint('https://cloud.appwrite.io/v1') # Your API Endpoint
