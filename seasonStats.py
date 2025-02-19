53f16d0240b3fb4efed58a17e27d01a7433cafb1
Desigan Reddy <78895361+Rebel1124@users.noreply.github.com>
2025-02-19 16:14:38 +0200

    Update seasonStats.py

--- seasonStats.py
+++ seasonStats.py
@@ -18,6 +18,7 @@ def main(context):
     project_id = os.environ['APPWRITE_PROJECT_ID']
     api_key = os.environ['APPWRITE_API_KEY']
     database_id = os.environ['APPWRITE_DB_ID']
+    footy_stats_key=os.environ['FOOTY_STATS_KEY']
     seasonStats_collection_id = os.environ['SEASON_STATS_COLLECTION_ID']
     
     client = (client
@@ -41,7 +42,7 @@ def main(context):
     def get_season_stats(leagueID):
         url = "https://api.football-data-api.com/league-season"
         paramsLeague = {
-            "key": "2d175fcc2f3c25f0a2a6f27e9ff2c662ca850cdc2fdac707296be94f83ddd837",
+            "key": footy_stats_key,
             "season_id": str(leagueID)
         }
         try:
