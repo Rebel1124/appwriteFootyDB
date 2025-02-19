15dbd77de4a93380be07b62607057faf591338ae
Desigan Reddy <78895361+Rebel1124@users.noreply.github.com>
2025-02-19 16:12:03 +0200

    Update leagueList.py

--- leagueList.py
+++ leagueList.py
@@ -19,6 +19,7 @@ def main(context):
     api_key = os.environ['APPWRITE_API_KEY']
     database_id = os.environ['APPWRITE_DB_ID']
     leagueList_collection_id = os.environ['LEAGUE_LIST_COLLECTION_ID']
+    footy_stats_key=os.environ['FOOTY_STATS_KEY']
     
     client = (client
         .set_endpoint('https://cloud.appwrite.io/v1') # Your API Endpoint
@@ -32,7 +33,7 @@ def main(context):
     def get_league_list():
         url = "https://api.football-data-api.com/league-list"
         paramsLeague = {
-            "key": "2d175fcc2f3c25f0a2a6f27e9ff2c662ca850cdc2fdac707296be94f83ddd837",
+            "key": footy_stats_key,
             "chosen_leagues_only": "true"
         }
         try:
