4f7815da1dbfeb50a8f236bf70d33ebdecc52151
Desigan Reddy <78895361+Rebel1124@users.noreply.github.com>
2025-02-19 16:11:10 +0200

    Update countryList.py

--- countryList.py
+++ countryList.py
@@ -19,7 +19,7 @@ def main(context):
     api_key = os.environ['APPWRITE_API_KEY']
     database_id = os.environ['APPWRITE_DB_ID']
     countryList_collection_id = os.environ['COUNTRY_LIST_COLLECTION_ID']
-    
+    footy_stats_key=os.environ['FOOTY_STATS_KEY']
     
     client = (client
         .set_endpoint('https://cloud.appwrite.io/v1') # Your API Endpoint
@@ -33,7 +33,7 @@ def main(context):
     def get_country_list():
         url = "https://api.football-data-api.com/country-list"
         params = {
-        "key": "2d175fcc2f3c25f0a2a6f27e9ff2c662ca850cdc2fdac707296be94f83ddd837"
+        "key": footy_stats_key
         }
         try:
             response = requests.get(url, params=params)
