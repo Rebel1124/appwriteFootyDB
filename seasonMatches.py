9c949256d6aeb88e6d50c11bc4d5fed2e2742198
Desigan Reddy <78895361+Rebel1124@users.noreply.github.com>
2025-02-19 16:13:51 +0200

    Update seasonMatches.py

--- seasonMatches.py
+++ seasonMatches.py
@@ -18,7 +18,9 @@ def main(context):
     project_id = os.environ['APPWRITE_PROJECT_ID']
     api_key = os.environ['APPWRITE_API_KEY']
     database_id = os.environ['APPWRITE_DB_ID']
+    footy_stats_key=os.environ['FOOTY_STATS_KEY']
     seasonMatches_collection_id = [os.environ['SEASON_MATCHES_EPL24_25'], os.environ['SEASON_MATCHES_PSL24_25']]
+    
 
     # seasonMatches_collection_id = [os.environ['SEASON_MATCHES_EPL20_21'],
     #                                os.environ['SEASON_MATCHES_EPL21_22'],
@@ -51,7 +53,7 @@ def main(context):
     def get_league_matches(leagueID, games=500):
         url = "https://api.football-data-api.com/league-matches"
         paramsLeague = {
-            "key": "2d175fcc2f3c25f0a2a6f27e9ff2c662ca850cdc2fdac707296be94f83ddd837",
+            "key": footy_stats_key,
             "season_id": str(leagueID),
             "max_per_page": str(games)
         }
