b00d00bf9df55f1f0e40f4e6b3a36197b92a4a97
Desigan Reddy <78895361+Rebel1124@users.noreply.github.com>
2025-02-21 03:45:24 +0200

    Update teamLast10_EPL.py

--- teamLast10_EPL.py
+++ teamLast10_EPL.py
@@ -23,7 +23,7 @@ def main(context):
     api_key = os.environ['APPWRITE_API_KEY']
     database_id = os.environ['APPWRITE_DB_ID']
     footy_stats_key=os.environ['FOOTY_STATS_KEY']
-    LASTX_collection_id = os.environ['LAST10_EPL24_25']
+    lastx_collection_id = os.environ['LAST10_EPL24_25']
     
     
     client = (client
