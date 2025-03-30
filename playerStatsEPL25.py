e72ff655601089660caeb2c375177455c723828d
Desigan Reddy <78895361+Rebel1124@users.noreply.github.com>
2025-03-30 06:54:45 +0200

    Update playerStatsEPL25.py

--- playerStatsEPL25.py
+++ playerStatsEPL25.py
@@ -368,7 +368,7 @@ def main(context):
     for i in range(0,count):
     
         # Retrieve league players
-        for j in range(1,6):
+        for j in range(1,11):
             try:
                 leaguePlayers_data = get_league_players(leagueID=currSeasonID[i], pageNum=j)
     
